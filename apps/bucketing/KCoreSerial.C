576553ef823e4a7eb64df9f8fde8b250a947654e
Stephen Yang <stephy.yang@mail.utoronto.ca>
2023-12-02 20:06:14 +0000

    update kcore, kcoreserial to have uniform outputs with other kcore apps

--- apps/bucketing/KCoreSerial.C
+++ apps/bucketing/KCoreSerial.C
@@ -3,14 +3,17 @@
 // Linear-work sequential implementation of the BZ coreness alg
 // Modified code from https://github.com/athomo/kcore/blob/master/src/KCoreWG_BZ.java
 
-struct decompress_f {
-  intT* deg;
-  intT* pos;
-  intT* vert;
-  intT* bin;
-  decompress_f(intT* _deg, intT* _pos, intT* _vert, intT* _bin) : deg(_deg), pos(_pos), vert(_vert), bin(_bin) {}
+struct decompress_f
+{
+  intT * deg;
+  intT * pos;
+  intT * vert;
+  intT * bin;
+  decompress_f(intT * _deg, intT * _pos, intT * _vert, intT * _bin)
+  : deg(_deg), pos(_pos), vert(_vert), bin(_bin) {}
 
-  inline bool update (const uintE& v, const uintE& u) {
+  inline bool update(const uintE & v, const uintE & u)
+  {
     if (deg[u] > deg[v]) {
       intT du = deg[u]; intT pu = pos[u];
       intT pw = bin[du]; intT w = vert[pw];
@@ -24,32 +27,34 @@ struct decompress_f {
     return false;
   }
 
-  inline bool updateAtomic (uintE s, uintE d) {
+  inline bool updateAtomic(uintE s, uintE d)
+  {
     return update(s, d);
   }
 
   // Condition is checked in update.
-  inline bool cond (uintE d) { return true; }
+  inline bool cond(uintE d) {return true;}
 };
 
-template <class vertex>
-array_imap<intT> KCore(graph<vertex>& G, bool printCores=false) {
+template<class vertex>
+array_imap<intT> KCore(graph<vertex> & G, bool printCores = false)
+{
   size_t n = G.n, m = G.m;
 
   uintE md = 0;
-  for(size_t v=0; v<n; v++) {
+  for (size_t v = 0; v < n; v++) {
     md = max(md, (uintE)G.V[v].getOutDegree());
   }
 
   array_imap<intT> vert(n);
   array_imap<intT> pos(n);
   array_imap<intT> deg(n);
-  array_imap<intT> bin(md+1);
+  array_imap<intT> bin(md + 1);
 
-  for (size_t d=0; d<=md; d++) {
+  for (size_t d = 0; d <= md; d++) {
     bin[d] = 0;
   }
-  for (size_t v=0; v<n; v++) {
+  for (size_t v = 0; v < n; v++) {
     intT d_v = G.V[v].getOutDegree();
     deg[v] = d_v;
     bin[d_v]++;
@@ -57,22 +62,22 @@ array_imap<intT> KCore(graph<vertex>& G, bool printCores=false) {
 
   // Scan for positions.
   size_t start = 0;
-  for (size_t d=0; d<=md; d++) {
+  for (size_t d = 0; d <= md; d++) {
     intT num = bin[d];
     bin[d] = start;
     start += num;
   }
 
   // Bin-sort.
-  for (size_t v=0; v<n; v++) {
+  for (size_t v = 0; v < n; v++) {
     pos[v] = bin[deg[v]];
     vert[pos[v]] = v;
     bin[deg[v]]++;
   }
 
   // Recover bin[].
-  for (size_t d=md; d>=1; d--) {
-    bin[d] = bin[d-1];
+  for (size_t d = md; d >= 1; d--) {
+    bin[d] = bin[d - 1];
   }
   bin[0] = 0;
 
@@ -88,21 +93,22 @@ array_imap<intT> KCore(graph<vertex>& G, bool printCores=false) {
   vs.del();
 
   if (printCores) {
-    for (size_t i=0; i<n; i++) {
+    for (size_t i = 0; i < n; i++) {
       cout << deg[i] << endl;
     }
   }
   size_t mc = 0;
-  for (size_t i=0; i<n; i++) {
+  for (size_t i = 0; i < n; i++) {
     mc = max(mc, (size_t)deg[i]);
   }
-  cout << "### max core: " << mc << endl;
+  cout << "largestCore was " << mc << endl;
 
   return deg;
 }
 
-template <class vertex>
-void Compute(graph<vertex>& GA, commandLine P) {
+template<class vertex>
+void Compute(graph<vertex> & GA, commandLine P)
+{
   bool printCores = P.getOptionValue("-p");
   cout << "### application: k-core-serial" << endl;
   cout << "### graph: " << P.getArgument(0) << endl;
@@ -110,4 +116,10 @@ void Compute(graph<vertex>& GA, commandLine P) {
   cout << "### n: " << GA.n << endl;
   cout << "### m: " << GA.m << endl;
   auto cores = KCore(GA, printCores);
+  if (printCores) {
+    cout << "cores: " << endl;
+    for (int i = 0; i < GA.n; i++) {
+      cout << cores[i] << endl;
+    }
+  }
 }
